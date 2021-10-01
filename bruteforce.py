48b9f6af8b213c1a84dc9373384edafd9928417a
Orphée <letembeorphee@gmail.com>
2021-10-02 00:08:15 +0200

    add function trifusion

--- bruteforce.py
+++ bruteforce.py
@@ -1,98 +1,9 @@
 
+import csv
 
-tab_actions = [
-    {"name": "action-1",
-     "cost": 20,
-     "benefit": 0.05},
+lecteur = csv.DictReader(open("action_list.csv", "r"))
+tab_actions = [dict(ligne) for ligne in lecteur]
 
-    {"name": "action-2",
-     "cost": 30,
-     "benefit": 0.10},
-
-    {"name": "action-3",
-     "cost": 50,
-     "benefit": 0.15},
-
-    {"name": "action-4",
-     "cost": 70,
-     "benefit": 0.20},
-
-    {"name": "action-5",
-     "cost": 60,
-     "benefit": 0.17},
-
-    {"name": "action-6",
-     "cost": 80,
-     "benefit": 0.25},
-
-    {"name": "action-7",
-     "cost": 22,
-     "benefit": 0.07},
-
-    {"name": "action-8",
-     "cost": 26,
-     "benefit": 0.11},
-
-    {"name": "action-9",
-     "cost": 48,
-     "benefit": 0.13},
-
-    {"name": "action-10",
-     "cost": 34,
-     "benefit": 0.27},
-
-    {"name": "action-11",
-     "cost": 42,
-     "benefit": 0.17},
-
-    {"name": "action-12",
-     "cost": 110,
-     "benefit": 0.09},
-
-    {"name": "action-13",
-     "cost": 38,
-     "benefit": 0.23},
-
-    {"name": "action-14",
-     "cost": 14,
-     "benefit": 0.01},
-
-    {"name": "action-15",
-     "cost": 18,
-     "benefit": 0.03},
-
-    {"name": "action-16",
-     "cost": 8,
-     "benefit": 0.08},
-
-    {"name": "action-17",
-     "cost": 4,
-     "benefit": 0.12},
-
-    {"name": "action-18",
-     "cost": 10,
-     "benefit": 0.14},
-
-    {"name": "action-19",
-     "cost": 24,
-     "benefit": 0.21},
-
-    {"name": "action-20",
-     "cost": 114,
-     "benefit": 0.18},
-]
-
-
-
-
-"""
-1- trouver toutes les possibilités d'investissement 
-
-  un budget de 500
-2 - parmis toutes les possibilités évaluer celle qui offres 
-"""
-
-"""transformation du tableau des actions en binaire
 
 n = len(tab_actions)
 tab_integer = [i for i in range(2**n)]
@@ -110,14 +21,11 @@ for combinaison in tab_combinaisons:
     benefit_combinaison = 0
     for i in range(n):
         if combinaison[i] == "1":
-            cost_combinaison = cost_combinaison + tab_actions[i]["cost"]
-            benefit_combinaison = benefit_combinaison + (tab_actions[i]["cost"] * tab_actions[i]["benefit"])
+            cost_combinaison += int(tab_actions[i]["cost"])
+            benefit_combinaison += int(tab_actions[i]["cost"]) * (int(tab_actions[i]["benefit"]) / 100)
 
     if cost_combinaison <= celling:
         valid_combinations.append([combinaison, cost_combinaison, benefit_combinaison])
-print(valid_combinations[10:])
-
-"""
 
 
 def fusion(tab_1, tab_2):
@@ -128,7 +36,7 @@ def fusion(tab_1, tab_2):
         combi1 = tab_1[i]
         benefit_combi1 = tab_1[i][2]
         combi2 = tab_2[j]
-        benefit_combi2 = tab_2[i][2]
+        benefit_combi2 = tab_2[j][2]
         if benefit_combi1 <= benefit_combi2:
             tab_fusion.append(combi1)
             i = i + 1
@@ -147,29 +55,17 @@ def fusion(tab_1, tab_2):
     return tab_fusion
 
 
-tab1 = [['11111111110010001100', 492, 86.14000000000001],
-         ['11111111110010010000', 486, 84.9],
-         ['11111111110010010100', 496, 86.30000000000001],
-         ['11111111110010011000', 490, 85.38000000000001],
-         ['11111111110010011100', 500, 86.78000000000002]]
-
-tab2 = [['11111111110010101000', 500, 85.28000000000002],
-        ['11111111110011000000', 492, 84.4],
-        ['11111111110011001000', 496, 84.88000000000001],
-        ['11111111110011010000', 500, 85.04],
-        ['11111111111000000000', 482, 82.66000000000001],
-        ['11111111111000000100', 492, 84.06000000000002],
-        ['11111111111000001000', 486, 83.14000000000001],
-        ['11111111111000001100', 496, 84.54000000000002],
-        ['11111111111000010000', 490, 83.30000000000001]]
+def tri_fusion(tab, start, end):
+    if start == end:
+        return [tab[start]]
+    else:
+        i = (end - start) // 2
+        tab_part1 = tri_fusion(tab, start, start + i)
+        tab_part2 = tri_fusion(tab, start + 1 + i, end)
+        return fusion(tab_part1, tab_part2)
 
 
-tab = fusion(tab1, tab2)
-print(tab)
-
-
-
-
+print(tri_fusion(valid_combinations, 0, len(valid_combinations) - 1))
 
 
 
