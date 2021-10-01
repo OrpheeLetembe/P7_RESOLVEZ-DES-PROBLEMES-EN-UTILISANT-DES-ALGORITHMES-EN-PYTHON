d8d5b420486853264b1f1df87206ac81d349039b
Orphée <letembeorphee@gmail.com>
2021-10-01 11:43:38 +0200

    add function fusion

--- bruteforce.py
+++ bruteforce.py
@@ -1,6 +1,6 @@
 
 
-list_actions = [
+tab_actions = [
     {"name": "action-1",
      "cost": 20,
      "benefit": 0.05},
@@ -84,60 +84,88 @@ list_actions = [
 
 
 
-"""
 
+"""
 1- trouver toutes les possibilités d'investissement 
 
   un budget de 500
 2 - parmis toutes les possibilités évaluer celle qui offres 
 """
 
+"""transformation du tableau des actions en binaire
 
+n = len(tab_actions)
+tab_integer = [i for i in range(2**n)]
 
-"""transformation du tableau des actions en binaire"""
+tab_bin = [bin(i)[2:] for i in tab_integer]
 
-n = len(list_actions)
-integer_list = [i for i in range(2**n)]
-
-binaire_list = [bin(i)[2:] for i in integer_list]
-
-all_combinaisons = ["0"*(n-len(k)) + k for k in binaire_list]
-
-
-
+tab_combinaisons = ["0"*(n-len(k)) + k for k in tab_bin]
 
 
 celling = 500
 valid_combinations = []
 
-for combi in all_combinaisons:
-    cost_combi = 0
-    benefit_combi = 0
+for combinaison in tab_combinaisons:
+    cost_combinaison = 0
+    benefit_combinaison = 0
     for i in range(n):
-        if combi[i] == "1":
-            cost_combi = cost_combi + list_actions[i]["cost"]
-            benefit_combi = benefit_combi + (list_actions[i]["cost"] * list_actions[i]["benefit"])
+        if combinaison[i] == "1":
+            cost_combinaison = cost_combinaison + tab_actions[i]["cost"]
+            benefit_combinaison = benefit_combinaison + (tab_actions[i]["cost"] * tab_actions[i]["benefit"])
 
-    if cost_combi <= celling:
-        valid_combinations.append([combi, cost_combi, benefit_combi])
+    if cost_combinaison <= celling:
+        valid_combinations.append([combinaison, cost_combinaison, benefit_combinaison])
+print(valid_combinations[10:])
 
-
-best_investment = valid_combinations[0][0]
-best_cost = valid_combinations[0][1]
-best_benefit = valid_combinations[0][2]
-for combi in valid_combinations:
-    if combi[1] > best_benefit:
-        best_investment = combi[0]
-        best_cost = combi[1]
-        best_benefit = combi[2]
-
-list_op = []
-for i in range(len(best_investment)):
-    if best_investment[i] == "1":
-        list_op.append(list_actions[i]["name"])
+"""
 
 
-print(list_op)
+def fusion(tab_1, tab_2):
+    tab_fusion = []
+    i = 0
+    j = 0
+    while i < len(tab_1) and j < len(tab_2):
+        combi1 = tab_1[i]
+        benefit_combi1 = tab_1[i][2]
+        combi2 = tab_2[j]
+        benefit_combi2 = tab_2[i][2]
+        if benefit_combi1 <= benefit_combi2:
+            tab_fusion.append(combi1)
+            i = i + 1
+        else:
+            tab_fusion.append(combi2)
+            j = j + 1
+        if i == len(tab_1):
+            while j < len(tab_2):
+                tab_fusion.append(tab_2[j])
+                j = j + 1
+        elif j == len(tab_2):
+            while i < len(tab_1):
+                tab_fusion.append(tab_1[i])
+                i = i + 1
+
+    return tab_fusion
+
+
+tab1 = [['11111111110010001100', 492, 86.14000000000001],
+         ['11111111110010010000', 486, 84.9],
+         ['11111111110010010100', 496, 86.30000000000001],
+         ['11111111110010011000', 490, 85.38000000000001],
+         ['11111111110010011100', 500, 86.78000000000002]]
+
+tab2 = [['11111111110010101000', 500, 85.28000000000002],
+        ['11111111110011000000', 492, 84.4],
+        ['11111111110011001000', 496, 84.88000000000001],
+        ['11111111110011010000', 500, 85.04],
+        ['11111111111000000000', 482, 82.66000000000001],
+        ['11111111111000000100', 492, 84.06000000000002],
+        ['11111111111000001000', 486, 83.14000000000001],
+        ['11111111111000001100', 496, 84.54000000000002],
+        ['11111111111000010000', 490, 83.30000000000001]]
+
+
+tab = fusion(tab1, tab2)
+print(tab)
 
 
 
