0d7ddac8278c5b59833a2decbd2f2c80aae281a4
Orphée <letembeorphee@gmail.com>
2021-10-03 19:37:26 +0200

    delete tri fusion

--- bruteforce.py
+++ bruteforce.py
@@ -10,7 +10,12 @@ def file_conversion_to_dict(file):
     return [dict(line) for line in reader]
 
 
-def dict_conversion_to_bin(dict_actions):
+def get_all_combination(dict_actions):
+    """
+
+    :param dict_actions:
+    :return:
+    """
     n = len(dict_actions)
     tab_integer = [i for i in range(2**n)]
     tab_bin = [bin(i)[2:] for i in tab_integer]
@@ -32,46 +37,25 @@ def get_valid_investment(tab1, tab2):
     return valid_investment
 
 
-def fusion(tab_1, tab_2):
-    tab_fusion = []
-    i = 0
-    j = 0
-    while i < len(tab_1) and j < len(tab_2):
-        first_combination = tab_1[i]
-        second_combination = tab_2[j]
-        if first_combination[2] <= second_combination[2]:
-            tab_fusion.append(first_combination)
-            i = i + 1
-        else:
-            tab_fusion.append(second_combination)
-            j = j + 1
-        if i == len(tab_1):
-            while j < len(tab_2):
-                tab_fusion.append(tab_2[j])
-                j = j + 1
-        elif j == len(tab_2):
-            while i < len(tab_1):
-                tab_fusion.append(tab_1[i])
-                i = i + 1
+def get_best_investment(tab):
 
-    return tab_fusion
+    best_investment = tab[0][0]
+    best_investment_cost = tab[0][1]
+    best_investment_benefit = tab[0][2]
+    for investment in tab:
+        if investment[2] > best_investment_benefit:
+            best_investment = investment[0]
+            best_investment_cost = investment[1]
+            best_investment_benefit = investment[2]
 
-
-def tri_fusion(tab, start, end):
-    if start == end:
-        return [tab[start]]
-    else:
-        i = (end - start) // 2
-        tab_part1 = tri_fusion(tab, start, start + i)
-        tab_part2 = tri_fusion(tab, start + 1 + i, end)
-        return fusion(tab_part1, tab_part2)
+    return [best_investment, best_investment_cost, best_investment_benefit]
 
 
 def format_best_investment(investment, tab):
     best_actions_list = []
-    best_action_name = investment[0]
-    for i in range(len(best_action_name)):
-        if best_action_name[i] == "1":
+    action_name = investment[0]
+    for i in range(len(action_name)):
+        if action_name[i] == "1":
             best_actions_list.append(tab[i]["name"])
 
     print("Best investment : actions: {}, Cost : {}€,  Benefit : {}€ ".format(best_actions_list, investment[1],
@@ -80,15 +64,10 @@ def format_best_investment(investment, tab):
 
 def main():
     tab_actions = file_conversion_to_dict("action_list.csv")
-    print(tab_actions)
-
-
-    """
-    tab_investments = dict_conversion_to_bin(tab_actions)
+    tab_investments = get_all_combination(tab_actions)
     valid_investments = get_valid_investment(tab_investments, tab_actions)
-    best_investment = tri_fusion(valid_investments, 0, len(valid_investments) - 1)[-1]
+    best_investment = get_best_investment(valid_investments)
     format_best_investment(best_investment, tab_actions)
-    """
 
 
 if __name__ == "__main__":
