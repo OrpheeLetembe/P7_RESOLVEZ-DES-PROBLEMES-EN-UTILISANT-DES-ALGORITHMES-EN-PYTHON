42ec6b2cb92eacc2e1b0e1cf9c2db947f1681a36
Orphée <letembeorphee@gmail.com>
2021-10-03 12:15:46 +0200

    add function

--- bruteforce.py
+++ bruteforce.py
@@ -1,33 +1,35 @@
-
+#! /user/bin/env python3
+# -*- coding: utf-8 -*-
 import csv
 
-reader = csv.DictReader(open("action_list.csv", "r"))
-tab_actions = [dict(line) for line in reader]
+CELLING = 500
 
 
-n = len(tab_actions)
+def file_conversion_to_dict(file):
+    reader = csv.DictReader(open(file, "r"))
+    return [dict(line) for line in reader]
 
 
-tab_integer = [i for i in range(2**n)]
+def dict_conversion_to_bin(dict_actions):
+    n = len(dict_actions)
+    tab_integer = [i for i in range(2**n)]
+    tab_bin = [bin(i)[2:] for i in tab_integer]
+    return ["0"*(n-len(k)) + k for k in tab_bin]
 
-tab_bin = [bin(i)[2:] for i in tab_integer]
 
-tab_investments = ["0"*(n-len(k)) + k for k in tab_bin]
+def get_valid_investment(tab1, tab2):
+    valid_investment = []
+    for investment in tab1:
+        cost_investment = 0
+        benefit_investment = 0
+        for i in range(len(tab2)):
+            if investment[i] == "1":
+                cost_investment += int(tab2[i]["cost"])
+                benefit_investment += int(tab2[i]["cost"]) * (int(tab2[i]["benefit"]) / 100)
 
-
-celling = 500
-valid_investments = []
-
-for investment in tab_investments:
-    cost_investment = 0
-    benefit_investment = 0
-    for i in range(n):
-        if investment[i] == "1":
-            cost_investment += int(tab_actions[i]["cost"])
-            benefit_investment += int(tab_actions[i]["cost"]) * (int(tab_actions[i]["benefit"]) / 100)
-
-    if cost_investment <= celling:
-        valid_investments.append([investment, cost_investment, benefit_investment])
+        if cost_investment <= CELLING:
+            valid_investment.append([investment, cost_investment, benefit_investment])
+    return valid_investment
 
 
 def fusion(tab_1, tab_2):
@@ -65,14 +67,24 @@ def tri_fusion(tab, start, end):
         return fusion(tab_part1, tab_part2)
 
 
-best_investment = tri_fusion(valid_investments, 0, len(valid_investments) - 1)[-1]
+def format_best_investment(investment, tab):
+    best_actions_list = []
+    best_action_name = investment[0]
+    for i in range(len(best_action_name)):
+        if best_action_name[i] == "1":
+            best_actions_list.append(tab[i]["name"])
 
-best_actions_list = []
-best_action_name = best_investment[0]
-for i in range(len(best_action_name)):
-    if best_action_name[i] == "1":
-        best_actions_list.append(tab_actions[i]["name"])
+    print("Best investment : actions: {}, Cost : {}€,  Benefit : {}€ ".format(best_actions_list, investment[1],
+                                                                              round(investment[2], 2)))
 
 
-print("Best investment : actions: {}, Cost : {}€,  Benefit : {}€ ".format(best_actions_list, best_investment[1],
-                                                                          round(best_investment[2], 2)))
+def main():
+    tab_actions = file_conversion_to_dict("action_list.csv")
+    tab_investments = dict_conversion_to_bin(tab_actions)
+    valid_investments = get_valid_investment(tab_investments, tab_actions)
+    best_investment = tri_fusion(valid_investments, 0, len(valid_investments) - 1)[-1]
+    format_best_investment(best_investment, tab_actions)
+
+
+if __name__ == "__main__":
+    main()
